beb8c73750d350ff6b380d288b4fa0e9ebf7ce8f
mestrey <yovan@ecovolve.fr>
2023-10-06 11:02:18 +0300

    Clean unused files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
     dirname(__DIR__)
@@ -107,9 +107,10 @@ $app->configure('app');
 */
 
 $app->router->group([
+    'prefix' => 'api/v' . env('APP_VERSION', 1),
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
-    require __DIR__.'/../routes/web.php';
+    require __DIR__ . '/../routes/web.php';
 });
 
 return $app;
